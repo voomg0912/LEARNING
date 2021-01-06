16b0866f291cdb3d92a4781ba84cb6f40a7c5f89
Myung-ki Lee <voomg@outlook.com>
2021-01-06 17:23:47 +0900

    11

---
+++ src/routes/Detail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location, history } = this.props;
+
+        if(location.state == undefined){
+            alert('영화정보가 없습니다.');
+            history.push('/');
+        }
+    }
+
+    render(){
+        const {location} = this.props;
+        if(location.state){
+            return <span>{location.state.title}</span>;
+        }else{
+            return null;
+        }
+    }
+}
+
+export default Detail;
